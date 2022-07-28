af9764c61c54f5344835191bb130fe6b5d6cf15b
Grammy2201 <gregorys234@gmail.com>
2022-07-28 12:09:51 -0500

    add java files

---
+++ main.js
@@ -0,0 +1,31 @@
+function totalBasketballScore ( freeThrows,midRange , threePointers){
+    return freeThrows*1 + midRange*2 + threePointers*3;
+}
+
+console.log(totalBasketballScore)
+
+
+
+
+function itemPrice(item){
+    switch(item) {
+        case 1:
+            return "$500"
+            case 2:
+                return "$10"
+                case 3:
+                    return "$250"
+                    case 4:
+                        return "$25"
+                        case 5:
+                            return "$100"
+    }
+    console.log(itemPrice)
+}
+
+
+function randomRestaurant(numOfRestaurants){
+    const randomRestaurantList=
+    ["Applebees", "PaneraBread", "Popeyes", "Chopt", "Chick-fil-A", "Cheescake Factory", "Five Guys" ];
+    }
+    return "randomRestaurantList"
